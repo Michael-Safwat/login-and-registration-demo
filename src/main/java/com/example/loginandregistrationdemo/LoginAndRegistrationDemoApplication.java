07548785a1426c88ec0687919b852d58f0afb3e1
Michael Safwat <mikesafwt34@gmail.com>
2024-08-13 18:49:11 +0300

    project created

---
+++ src/main/java/com/example/loginandregistrationdemo/LoginAndRegistrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginandregistrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAndRegistrationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loginandregistrationdemo/LoginAndRegistrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginandregistrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
